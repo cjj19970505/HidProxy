165a40fde01cb71695090bf5117cacb16cdd523a
陈俊嘉 <cjj19970505@live.cn>
2025-02-28 01:34:34 +0800

    Remove outdated projectes
    Remove LibHidp and libHidpTest.
    Add comment to the inverted call model.

--- HidProxy/NotificationQueue.h
+++ HidProxy/NotificationQueue.h
@@ -2,4 +2,7 @@
 #include <ntddk.h>
 #include <wdf.h>
 
+// Inspired by Inverted call model
+// https://www.osr.com/nt-insider/2013-issue1/inverted-call-model-kmdf/
+// https://github.com/OSRDrivers/Inverted
 NTSTATUS HidProxyNotificationQueueInitialize(WDFFILEOBJECT FileObject, WDFQUEUE* Queue);
